551b86f039d86dd3923e42e93a89716cd79c87f8
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2023-01-18 22:42:01 +0100

    Update index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -8,9 +8,18 @@ import cors from "cors";
 import helmet from "helmet";
 
 dotenv.config();
+
 /**
  * App Variables
  */
+
+if (!process.env.PORT) {
+  process.exit(1);
+}
+
+const PORT: number = parseInt(process.env.PORT as string, 10);
+
+const app = express();
 
 /**
  *  App Configuration
