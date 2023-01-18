2ea0803a3cd3145ef3c0c6e44eb8270d1d679caf
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2023-01-18 22:39:46 +0100

    Update index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app: Express = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
