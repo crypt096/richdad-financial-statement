d86ea7effe8cfbae1acafd9f708dd61ca0c377f0
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2023-01-18 22:41:10 +0100

    Add template

--- backend/index.ts
+++ backend/index.ts
@@ -1,15 +1,15 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+/**
+ * Required External Modules
+ */
 
-dotenv.config();
+/**
+ * App Variables
+ */
 
-const app: Express = express();
-const port = process.env.PORT;
+/**
+ *  App Configuration
+ */
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
-
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+/**
+ * Server Activation
+ */
