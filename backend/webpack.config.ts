a03ea2333e9f35cc8177f65226d590aea90ec27e
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2023-01-19 11:44:23 +0100

    Change webpack config

--- backend/webpack.config.ts
+++ backend/webpack.config.ts
@@ -1,32 +1,15 @@
-const webpack = require("webpack");
-const path = require("path");
-const nodeExternals = require("webpack-node-externals");
+import * as path from 'path';
+import * as webpack from 'webpack';
+// in case you run into any typescript error when configuring `devServer`
+import 'webpack-dev-server';
 
-module.exports = {
-  entry: ["webpack/hot/poll?100", "./src/index.ts"],
-  watch: true,
-  target: "node",
-  externals: [
-    nodeExternals({
-      whitelist: ["webpack/hot/poll?100"]
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/
-      }
-    ]
-  },
-  mode: "development",
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"]
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
+const config: webpack.Configuration = {
+  mode: 'production',
+  entry: './foo.js',
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: "index.js"
-  }
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'foo.bundle.js',
+  },
 };
+
+export default config;
