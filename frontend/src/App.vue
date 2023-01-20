1030cac9fc33f286a8fa97658f6d8c2890feb331
kristinavd <kristina.vidakovic403@gmail.com>
2023-01-20 17:13:50 +0100

    Added Income, Expenses and Auditor

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
+import IncomeStatement from "./components/IncomeStatement.vue";
+import Auditor from "./components/Auditor.vue";
+import Expenses from "./components/Expenses.vue";
 </script>
 
 <template>
-  <header>
-  </header>
-
-  <main>
-    <h1 class="text-5xl font-bold underline">
-      Hello world!
-    </h1>
+  <main class="m-10 border border-sky-500">
+    <div class="mt-8 ml-3 mb-3 text-xl">
+        INCOME STATEMENT
+    </div>
+    <div class="grid grid-cols-2">
+      <IncomeStatement/>
+      <Auditor class="row-span-2"/>
+      <Expenses class="mt-5"/>
+    </div>
   </main>
-
-  <footer>
-  </footer>
 </template>
---
+++ frontend/src/components/IncomeStatement.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+    <main>
+        <div class="grid grid-cols-2 mx-3 pb-3">
+            <div class="col-span-2 grid justify-items-center bg-sky-100 font-bold">INCOME</div>
+            <div class="grid justify-items-center font-bold">Description</div>
+            <div class="grid justify-items-center font-bold">Cash Flow</div>
+            <div class="grid justify-items-start border-b border-sky-900 mr-3 pl-3">Salary</div>
+            <div class="grid justify-items-center border-b border-sky-900 ml-3">0</div>
+            <div class="grid justify-items-start border-b border-sky-900 mr-3 pl-3">Interest/Dividends</div>
+            <div class="grid justify-items-center border-b border-sky-900 ml-3">0</div>
+            <div class="grid justify-items-start border-b border-sky-900 mr-3 pl-3">Real Estate/Businesses</div>
+            <div class="grid justify-items-center border-b border-sky-900 ml-3">0</div>
+        </div>
+    </main>
+</template>
