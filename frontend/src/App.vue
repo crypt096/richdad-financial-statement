7c61710511426621484ea22c83301f80fad7e26a
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2023-02-14 19:56:06 -0500

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,9 @@
 <template>
-  <main class="m-10 border border-sky-500">
-    <div class="mt-8 ml-3 mb-3 text-xl">INCOME STATEMENT</div>
-    <!-- <div class="grid grid-cols-2">
-      <income-statement></income-statement>
-      <auditor></auditor>
-      <expenses></expenses>
-    </div> -->
+  <main>
+    <!-- Put components here -->
+    
   </main>
 </template>
 
 <script setup lang="ts">
-import IncomeStatement from "./components/IncomeStatement.vue";
-import Auditor from "./components/Auditor.vue";
-import Expenses from "./components/Expenses.vue";
 </script>
