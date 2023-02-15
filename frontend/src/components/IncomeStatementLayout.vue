6e2a31013ca54dcf1456fc998c1d2bcd90df8ecd
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2023-02-14 20:13:01 -0500

    Update IncomeStatementLayout.vue

--- frontend/src/components/IncomeStatementLayout.vue
+++ frontend/src/components/IncomeStatementLayout.vue
@@ -2,8 +2,10 @@
   <!-- Create layout like this: https://www.richdad.com/MediaLibrary/RichDad/images/blogs/income-statement-131003.png -->
   <!-- I've just added components for you here. Add appropriate classes and make it look like in the image above -->
   <income></income>
+  <expenses></expenses>
 </template>
 
 <script lang="ts" setup>
 import Income from "./Income.vue";
+import Expenses from "./Expenses.vue";
 </script>
