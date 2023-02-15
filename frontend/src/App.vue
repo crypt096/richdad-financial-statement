ac12d7d8392c960776fcb367cf583fe2e3582b1a
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2023-02-14 19:51:22 -0500

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,13 +6,11 @@ import Expenses from "./components/Expenses.vue";
 
 <template>
   <main class="m-10 border border-sky-500">
-    <div class="mt-8 ml-3 mb-3 text-xl">
-        INCOME STATEMENT
-    </div>
+    <div class="mt-8 ml-3 mb-3 text-xl">INCOME STATEMENT</div>
     <div class="grid grid-cols-2">
-      <IncomeStatement/>
-      <Auditor class="row-span-2"/>
-      <Expenses class="mt-5"/>
+      <IncomeStatement />
+      <Auditor class="row-span-2" />
+      <Expenses class="mt-5" />
     </div>
   </main>
 </template>
