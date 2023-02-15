fab39b7b5be9ffe9240bd9e8d42fee05e06d93e6
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2023-02-14 20:07:06 -0500

    Update IncomeStatementLayout.vue

--- frontend/src/components/IncomeStatementLayout.vue
+++ frontend/src/components/IncomeStatementLayout.vue
@@ -1,4 +1,6 @@
 <template>
+  <!-- Create layout like this: https://www.richdad.com/MediaLibrary/RichDad/images/blogs/income-statement-131003.png -->
+  <!-- I've just added components for you here. Add appropriate classes and make it look like in the image above -->
   <income></income>
 </template>
 
