4b7974c7eaf5240f1aec426508cea5fc656874de
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2023-02-20 18:50:19 -0500

    Update FinancialStatementLayout.vue

--- frontend/src/components/FinancialStatementLayout.vue
+++ frontend/src/components/FinancialStatementLayout.vue
@@ -1,5 +1,6 @@
 <template>
   <main class="mt-5 m-4 border border-dark border-2">
+    <div class="fs-4 text-uppercase pt-3 ps-3 fw-semibold">Income Statement</div>
     Financial statement Layout component
   </main>
 </template>
