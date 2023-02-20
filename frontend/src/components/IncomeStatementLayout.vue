840d71e9533c74aac66f25ff678ce8915898c3b0
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2023-02-20 10:17:53 -0500

    Update IncomeStatementLayout.vue

--- frontend/src/components/IncomeStatementLayout.vue
+++ frontend/src/components/IncomeStatementLayout.vue
@@ -24,7 +24,11 @@
     </div>
 
     <div>
-      <button type="button" @click="showEditStatementModal()">
+      <button
+        type="button"
+        class="bg-sky-600 border-black rounded text-white px-4 py-2"
+        @click="showEditStatementModal()"
+      >
         Edit statement
       </button>
     </div>
