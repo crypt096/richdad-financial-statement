5844c9423a8d4a0575eca40df74a9f2a4a203b76
kristinavd <kristina.vidakovic403@gmail.com>
2023-02-20 14:07:48 -0500

    Update Income.vue

--- frontend/src/components/Income.vue
+++ frontend/src/components/Income.vue
@@ -1,14 +1,27 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div class="grid grid-cols-2 mx-3 pb-3">
+  <div class="grid mx-3 pb-3">
     <div
-      class="col-span-2 grid bg-neutral-900 pl-3 text-neutral-50 py-1"
+      class="grid bg-neutral-900 pl-3 text-neutral-50 py-1"
     >
       INCOME
     </div>
-    <div class="grid justify-items-center text-sm border-b-2 border-black">Description</div>
-    <div class="grid justify-items-center text-sm border-b-2 border-black">Cash Flow</div>
-    <div class="grid justify-items-start border-b border-sky-900 mr-3">
+    <div class="flex">
+      <div class="flex justify-center text-sm border-b-2 border-black w-9/12">Description</div>
+      <div class="flex justify-center text-sm border-b-2 border-black w-3/12">Cash Flow</div>
+    </div>
+
+    <div class="flex">
+      <div class="flex text-sm border-b border-black w-9/12 mr-2">Salary</div>
+      <div class="flex justify-center text-sm border-b border-black w-3/12">0</div>
+    </div>
+
+
+
+
+
+
+    <!-- <div class="grid justify-items-start border-b border-sky-900 mr-3">
       Salary
     </div>
     <div class="grid justify-items-center border-b border-sky-900 ml-3">0</div>
@@ -19,6 +32,6 @@
     <div class="grid justify-items-start border-b border-sky-900 mr-3">
       Real Estate/Businesses
     </div>
-    <div class="grid justify-items-center border-b border-sky-900 ml-3">0</div>
+    <div class="grid justify-items-center border-b border-sky-900 ml-3">0</div> -->
   </div>
 </template>
