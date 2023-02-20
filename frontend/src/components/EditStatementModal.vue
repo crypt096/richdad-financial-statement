962e6f113510c890067e8f3135bb745339a292ba
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2023-02-20 16:08:40 -0500

    Update EditStatementModal.vue

--- frontend/src/components/EditStatementModal.vue
+++ frontend/src/components/EditStatementModal.vue
@@ -12,11 +12,9 @@
       class="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center flex"
     >
       <div class="relative w-auto my-6 mx-auto max-w-6xl">
-        <!--content-->
         <div
           class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none"
         >
-
           <div
             class="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t"
           >
