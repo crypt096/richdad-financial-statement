a5d2c157d5d22e20eb7c5746a864687e0de5c2ed
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2023-02-20 19:04:15 -0500

    Update FinancialStatementLayout.vue

--- frontend/src/components/FinancialStatementLayout.vue
+++ frontend/src/components/FinancialStatementLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="mt-5 m-4 border border-dark border-2">
+  <main class="mt-5 m-4 border border-dark border-2 position-relative">
     <div class="fs-4 text-uppercase pt-3 ps-3 fw-semibold">Income Statement</div>
     
     <!-- Rest of code goes here -->
