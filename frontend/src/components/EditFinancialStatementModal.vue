889da358723147a8621da7dd8ef09d6e26f8fe69
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2023-02-21 15:32:38 -0500

    Update EditFinancialStatementModal.vue

--- frontend/src/components/EditFinancialStatementModal.vue
+++ frontend/src/components/EditFinancialStatementModal.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <button type="button" class="btn btn-outline-dark mt-4" @click="showModal">Edit statement</button>
-    
+
     <!-- Bootstrap modal -->
     <div class="modal" tabindex="-1" ref="modal">
       <div class="modal-dialog">
@@ -11,7 +11,16 @@
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
-            Modal Body
+            <!-- Select -->
+            <div class="form-floating">
+              <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+                <option selected>Open this select menu</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+              <label for="floatingSelect">Works with selects</label>
+            </div>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">Close</button>
