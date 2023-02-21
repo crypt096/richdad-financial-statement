f53c9b4f7cab2edf397445dbf700815f46524864
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2023-02-20 19:10:16 -0500

    Add Modal.

---
+++ frontend/src/components/EditFinancialStatementModal.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <button @click="showModal">Show Modal</button>
+    
+    <!-- Bootstrap modal -->
+    <div class="modal" tabindex="-1" ref="modal">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Modal Title</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            Modal Body
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { Modal } from 'bootstrap';
+
+const modal = ref<HTMLDivElement | null>(null);
+
+const showModal = () => {
+  if (modal.value) {
+    const modalInstance = new Modal(modal.value);
+    modalInstance.show();
+  }
+};
+</script>
--- frontend/src/components/FinancialStatementLayout.vue
+++ frontend/src/components/FinancialStatementLayout.vue
@@ -1,8 +1,19 @@
+
+import EditFinancialStatementModal from './EditFinancialStatementModal.vue';
+
 <template>
   <main class="mt-5 m-4 border border-dark border-2 position-relative">
     <div class="fs-4 text-uppercase pt-3 ps-3 fw-semibold">Income Statement</div>
     
     <!-- Rest of code goes here -->
     Income, Auditor, Expenses, etc...
+
+    <div class="mt-5">
+      <edit-financial-statement-modal></edit-financial-statement-modal>
+    </div>
   </main>
 </template>
+
+<script setup lang="ts">
+import EditFinancialStatementModal from './EditFinancialStatementModal.vue';
+</script>
