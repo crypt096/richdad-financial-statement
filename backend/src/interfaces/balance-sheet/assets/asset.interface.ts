108d0f1720c452f5a57351f282e9008270113303
Alex <31933549+crypt096@users.noreply.github.com>
2023-03-18 19:31:53 +0100

    Update interfaces.

---
+++ backend/src/interfaces/balance-sheet/assets/asset.interface.ts
@@ -0,0 +1,6 @@
+import { Stock } from "./stock.interface";
+
+export interface Asset {
+  savings: number;
+  stocks: Stock[];
+}
---
+++ backend/src/interfaces/balance-sheet/assets/stock.interface.ts
@@ -0,0 +1,6 @@
+export interface Stock {
+  id: string;
+  name: string;
+  numberOfShares: number;
+  costSharePrice: number;
+}
