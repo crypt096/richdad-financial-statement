2a3f5b069ad1d6b2b7f1d77c5c15d940d27ee259
Alex <31933549+crypt096@users.noreply.github.com>
2023-03-18 19:27:38 +0100

    Update income.interface.ts

--- backend/src/interfaces/income-statement/income.interface.ts
+++ backend/src/interfaces/income-statement/income.interface.ts
@@ -1,8 +1,12 @@
+import { Business } from "./business.interface";
 import { Dividend } from "./dividend.interface";
 import { Interest } from "./interest.interface";
+import { RealEstate } from "./real-estate.interface";
 
 export interface Income {
   salary: number;
   interests: Interest[];
   dividends: Dividend[];
+  realEstates: RealEstate[];
+  businesses: Business[];
 }
