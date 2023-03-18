bf1b1f2d7b23c964df2123ca942fe02764e0572f
Alex <31933549+crypt096@users.noreply.github.com>
2023-03-18 17:36:34 +0100

    Update FinancialStatementLayout.vue

--- frontend/src/components/FinancialStatementLayout.vue
+++ frontend/src/components/FinancialStatementLayout.vue
@@ -1,6 +1,3 @@
-
-import EditFinancialStatementModal from './EditFinancialStatementModal.vue';
-
 <template>
   <main class="mt-5 m-4 border border-dark border-2 position-relative">
     <div class="fs-4 text-uppercase pt-3 ps-3 fw-semibold">Income Statement</div>
