34274e9fd04e9046e4b1b54b77b5116156c0f41c
Alex <31933549+crypt096@users.noreply.github.com>
2023-03-18 19:33:29 +0100

    Update asset.interface.ts

--- backend/src/interfaces/balance-sheet/assets/asset.interface.ts
+++ backend/src/interfaces/balance-sheet/assets/asset.interface.ts
@@ -1,6 +1,10 @@
+import { CD } from "./cd.interface";
+import { Fund } from "./fund.interface";
 import { Stock } from "./stock.interface";
 
 export interface Asset {
   savings: number;
   stocks: Stock[];
+  funds: Fund[];
+  cds: CD[];
 }
