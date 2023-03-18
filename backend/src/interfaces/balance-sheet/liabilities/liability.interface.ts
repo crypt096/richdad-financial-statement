69b81eb30af81204e7cc7806fe6bc8ea2efc48e2
Alex <31933549+crypt096@users.noreply.github.com>
2023-03-18 19:40:17 +0100

    Update liability.interface.ts

--- backend/src/interfaces/balance-sheet/liabilities/liability.interface.ts
+++ backend/src/interfaces/balance-sheet/liabilities/liability.interface.ts
@@ -1,7 +1,13 @@
+import { Business } from "./business.interface";
+import { RealEstate } from "./real-estate.interface";
+
 export interface Liability {
   homeMortgage: number;
   schoolLoans: number;
   carLoans: number;
   creditCards: number;
   retailDebt: number;
+  realEstates: RealEstate[];
+  businesses: Business[];
+  loan: number;
 }
