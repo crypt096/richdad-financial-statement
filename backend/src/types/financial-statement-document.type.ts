30ebc3292f0ee213194dde35906b0c7a9d0853fe
Alex <31933549+crypt096@users.noreply.github.com>
2023-03-19 17:29:48 +0100

    Create financial-statement-document.type.ts

---
+++ backend/src/types/financial-statement-document.type.ts
@@ -0,0 +1,22 @@
+import { Income } from "../interfaces/income-statement/income/income.interface";
+import { Expenses } from "../interfaces/income-statement/expenses/expenses.interface";
+import { Asset } from "../interfaces/balance-sheet/assets/asset.interface";
+import { Liability } from "../interfaces/balance-sheet/liabilities/liability.interface";
+
+export type FinancialStatement = Document & {
+  id: string;
+  incomeStatement: {
+    income: Income;
+    expenses: Expenses;
+  },
+  balanceSheet: {
+    assets: Asset;
+    liabilities: Liability;
+  },
+  passiveIncome: number;
+  totalIncome: number;
+  numberOfChildren: number;
+  perChildExpense: number;
+  totalExpenses: number;
+  monthlyCashflow: number;
+}
