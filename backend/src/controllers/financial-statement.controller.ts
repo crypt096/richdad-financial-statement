a6c362fba2cdf3b80141568ed995c769b7ee2376
Alex <31933549+crypt096@users.noreply.github.com>
2023-03-19 17:45:45 +0100

    Update financial-statement.controller.ts

--- backend/src/controllers/financial-statement.controller.ts
+++ backend/src/controllers/financial-statement.controller.ts
@@ -6,3 +6,9 @@ import { FinancialStatement, FinancialStatementInput } from "../models/financial
  * @desc Get all financial statement information
  * @return {Object} financial statement
  */
+
+const getAllFinancialStatement = async (req: Request, res: Response) => {
+  const financialStatement = await FinancialStatement.find().sort("-createdAt").exec();
+
+  return res.status(200).json({ data: financialStatement });
+}
