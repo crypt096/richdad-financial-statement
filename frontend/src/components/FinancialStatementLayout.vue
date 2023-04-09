b97ef059e30ce98ceb732b8c7b6cef102d924088
Alex <31933549+crypt096@users.noreply.github.com>
2023-04-09 11:41:54 +0200

    Update FinancialStatementLayout.vue

--- frontend/src/components/FinancialStatementLayout.vue
+++ frontend/src/components/FinancialStatementLayout.vue
@@ -2,8 +2,7 @@
   <main class="mt-5 m-4 border border-dark border-2 position-relative">
     <div class="fs-4 text-uppercase pt-3 ps-3 fw-semibold">Income Statement</div>
     
-    <!-- Rest of code goes here -->
-    Income, Auditor, Expenses, etc...
+    <income></income>
 
     <div class="mt-5">
       <edit-financial-statement-modal></edit-financial-statement-modal>
@@ -13,4 +12,5 @@
 
 <script setup lang="ts">
 import EditFinancialStatementModal from './EditFinancialStatementModal.vue';
+import Income from './income-statement/Income.vue';
 </script>
