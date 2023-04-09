e7ada3537e73f6af1025b48545b53e4fd7927e30
Alex <31933549+crypt096@users.noreply.github.com>
2023-04-09 11:52:24 +0200

    Update Income.vue

--- frontend/src/components/income-statement/Income.vue
+++ frontend/src/components/income-statement/Income.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="container-fluid text-center">
-    <div class="row">
+    <div class="row ms-2">
       <div class="col bg-dark text-uppercase text-white">
         Income
+      </div>
+
+      <div class="col bg-dark text-uppercase text-white">
+        Auditor part
       </div>
     </div>
   </div>
