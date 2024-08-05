20235ba4bf9411b091bc9b488a0c6700c2b3db0f
Alex <31933549+crypt096@users.noreply.github.com>
2024-08-05 22:32:06 +0200

    Update HomePage.vue

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,8 +1,12 @@
 <template>
   <ion-page>
     <ion-content>
-      <ion-card>
-        hello
+      <ion-card class="m-4 rounded-lg">
+        <ion-row>
+          <ion-col class="flex justify-center items-center">
+            <div class="text-4xl text-white">Rich Dad</div>
+          </ion-col>
+        </ion-row>
       </ion-card>
     </ion-content>
   </ion-page>
@@ -11,6 +15,9 @@
 <script setup lang="ts">
 import {
   IonPage,
-  IonContent
+  IonContent,
+  IonCard,
+  IonRow,
+  IonCol,
 } from '@ionic/vue';
 </script>
